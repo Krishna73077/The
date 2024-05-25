0eeaa23739dfe18b3cf589168f0a09d1c5373cd5
krishnayadav <krishna73077yadav@gmail.com>
2024-05-25 07:18:37 +0530

    update

---
+++ m.js
@@ -0,0 +1,12 @@
+const iphone = { 
+  product_name:'iphone 15pro max 1TB storage 5000mh battery backup',
+  rating:3/5,
+  price:149000,
+};
+
+function add() {
+ alert(' Successfuly added to cart');
+};
+function buy() {
+  confirm(' Are you sure to buy')
+}
